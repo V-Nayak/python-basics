1a0b7958eb9c1e2b2152339baa9f6dc0c3c64366
V-Nayak <vinayakpatil89@outlook.com>
2024-07-17 15:09:42 +0530

    Lines starting

---
+++ Unique programs.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter no 1:"))
+n2=int(input("Enter no 2:"))
+n3=int(input("Enter no 3:"))
+if (n1>n2) and (n1>n2):
+  print("Laregst no is:", n1)
+elif (n2>n1) and (n2>n3):
+  print("Largest no is:", n2)
+else :
+  print("largest no is:", n3)
